fbdffd4a24ab06a58dbf3ccd859c81da3d816fc4
Isuru Ariyarathna <isuruariyarathna2k00@gmail.com>
2024-10-16 19:52:37 +0530

    Initial Commit

---
+++ CommentNetwork/CommentNetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommentNetworkApp.swift
+//  CommentNetwork
+//
+//  Created by Isuru Ariyarathna on 2024-10-16.
+//
+
+import SwiftUI
+
+@main
+struct CommentNetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CommentNetworkTests/CommentNetworkTests.swift
@@ -0,0 +1,17 @@
+//
+//  CommentNetworkTests.swift
+//  CommentNetworkTests
+//
+//  Created by Isuru Ariyarathna on 2024-10-16.
+//
+
+import Testing
+@testable import CommentNetwork
+
+struct CommentNetworkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
